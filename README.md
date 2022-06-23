474f02b83572ef242ffe0b3898c3dab663cdb034
RonaldoBento <talesmileto295@gmail.com>
2022-06-22 21:43:07 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# aulas-de-javascript
+ Exercírcios básicos de JavaScript
---
+++ exercicios/ex.js01/script.js
@@ -0,0 +1,10 @@
+/*Script functions */
+var quebrada = false
+function mudaLampada(tipo) { 
+    if (!quebrada) {
+        document.getElementById("luz").src = tipo + ".jpg"
+        if (tipo == 'lampada-quebrada') {
+            quebrada = true
+        }
+    }   
+}
